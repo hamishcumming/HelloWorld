9e68e292e035d4acfe3edd285468a8901635d6b3
hamishcumming <h.c.cumming@gmail.com>
2019-08-19 19:40:16 +1200

    new line 5 comment

--- HelloWorld/src/HelloWorldExample.java
+++ HelloWorld/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// A new comment to merge
 		System.out.println("Hello World");
 	}
 
